b51f0cf695f7256db9a231cbcf25fa417b95d8ac
b.kabylov <b.kabylov>
2023-12-03 04:25:02 +0600

    Configured locale

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,20 @@
 import logo from './logo.svg';
 import './App.css';
 import SignIn from './pages/SignIn';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import Header from './components/Header';
 
 function App() {
   return (
    <div className='App'>
-    <SignIn/>
+    <Header/>
+    <BrowserRouter>
+     <Routes>
+      <Route path="/" Component={SignIn} ></Route>
+      <Route path="/sign-up" Component={SignUp} ></Route>
+     </Routes>
+    </BrowserRouter>
    </div>
   );
 }
--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,6 +1,9 @@
 import  React, { useState } from 'react';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useTranslation } from 'react-i18next';
+import LanguageSelect from '../components/LanguageSelect';
+import { useNavigate, useNavigation } from 'react-router-dom';
 
 
 
@@ -10,6 +13,8 @@ const defaultTheme = createTheme();
 export default function SignIn() {
   const [login, setLogin] = useState<string>();
   const [password, setPassword] = useState<string>();
+  const { t, i18n } = useTranslation();
+  const navigate = useNavigate();
   // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
   //   event.preventDefault();
   //   const data = new FormData(event.currentTarget);
@@ -23,28 +28,22 @@ export default function SignIn() {
 <section className="bg-gray-50 dark:bg-gray-900">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-          <a href="#" className="flex justify-center items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-            <a className='ml-80 text-red-300'>KG</a>
-            <a className='ml-5 text-orange-300'>TR</a>    
-          </a>
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              <h3 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Дипломдук ишти тандоо системасы
+              <h3 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white h-16">
+                  {t('app-title')}
               </h3>
               <form className="space-y-4 md:space-y-6" action="#">
                   <div>
-                      <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                      <input type="email" value={login} onChange={e => setLogin(e.target.value)} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="someone@manas.edu.kg"/>
+                      <input type="email" value={login} onChange={e => setLogin(e.target.value)} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={t('email-input')}/>
                   </div>
                   <div>
-                      <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Сырсөзү</label>
-                      <input type="password" value={password} onChange={e => setPassword(e.target.value)} name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+                      <input type="password" value={password} onChange={e => setPassword(e.target.value)} name="password" id="password" placeholder={t('password')} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
                   </div>
                   <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Кирүү
+                  {t('sign-in')}
                   </button>
                   <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                      Каттала элексизби? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Катталуу</a>
+                  {t('doesnt-sign-up')} <a onClick={() => navigate("/sign-up")} className="font-medium text-primary-600 hover:underline dark:text-primary-500">{t('sign-up')}</a>
                   </p>
               </form>
           </div>
